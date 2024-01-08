b04caac921f5fc24eafa603d193a42f9959f941b
cark <kssko134@learner.dvusd.org>
2024-01-08 09:29:32 -0700

    thing

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter da numba");
+            int num = 0; 
+            int.TryParse(Console.ReadLine(),out num);
+            Console.WriteLine(num*num);
+            Console.ReadLine();
+        }
+    }
+}
